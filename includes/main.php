8f35ceda5561b82fbbd425921dadd4c676f22705
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-01-27 15:07:13 +0100

    Fichier head.php

---
+++ includes/main.php
@@ -0,0 +1,2 @@
+<h1>GameLib</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor tenetur nam velit delectus, est repudiandae tempora accusantium ratione ullam voluptate mollitia sed temporibus nihil praesentium fugit iure voluptatibus? Dicta, perferendis?</p>
