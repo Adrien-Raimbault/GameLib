cee262c2cad927cd1e54c18157da19daa6c8337f
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-01-27 15:42:30 +0100

    inclusion nav.php dans main.php

--- includes/main.php
+++ includes/main.php
@@ -1,2 +1,5 @@
+<?php 
+require_once './includes/nav.php'
+?>
 <h1>GameLib</h1>
 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor tenetur nam velit delectus, est repudiandae tempora accusantium ratione ullam voluptate mollitia sed temporibus nihil praesentium fugit iure voluptatibus? Dicta, perferendis?</p>
