288d89353c775577f71023050d0e0bc41d0e93da
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-01-28 10:46:17 +0100

    Commentaire de la fonction dump()

---
+++ includes/formulaire.inc.php
@@ -0,0 +1,2 @@
+<?php
+include './includes/frmFormulaire.php' ;
---
+++ includes/frmFormulaire.php
@@ -0,0 +1,13 @@
+<form action="index.php?page=formulaire" method= "post">
+    <label for="nom">Nom :</label>
+    <input type="text" placeholder="votre nom" name="nom" id="nom">
+    <br />
+    <label for="prenom">Prénom :</label>
+    <input type="text" placeholder="votre prénom" name="prenom" id="prenom">
+    <br />
+    <label for="email">e-mail :</label>
+    <input type="text" placeholder="email@email.fr" name="email" id="email">
+    <br />
+    <input type="reset" value="Effacer">
+    <input type="submit" value="Clique-moi coquinou !">
+</form>
