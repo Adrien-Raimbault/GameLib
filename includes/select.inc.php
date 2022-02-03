d1f6fb627298f2401bf4b3532edae6da2e79a041
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-02-03 15:01:36 +0100

    stylisation formulaire et tableau

--- includes/select.inc.php
+++ includes/select.inc.php
@@ -9,11 +9,48 @@ try {
     $conn = new PDO("mysql:host=$serverName; dbname=$userDB", $userName, $userPassword);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $requete= $conn->prepare("SELECT * FROM utilisateurs");
+    $requete= $conn->prepare("SELECT * FROM utilisateurs ORDER BY utilisateurs.nom ASC");
     $requete->execute();
     $resultat =$requete->fetchAll(PDO::FETCH_ASSOC);
 
-    dump($resultat);
+    // dump($resultat);
+
+    $table="<table class='paleBlueRows'>";
+    $table.="<tr>";
+    $table.="<th>";
+    $table.="ID";
+    $table.="</th>";
+    $table.="<th>";
+    $table.="Nom";
+    $table.="</th>";
+    $table.="<th>";
+    $table.="Prenom";
+    $table.="</th>";
+    $table.="<th>";
+    $table.="Adresse email";
+    $table.="</th>";
+
+
+
+    $table.="</tr>";
+
+    for($i = 0; $i < COUNT($resultat); $i++){
+        // var_dump($resultat[$i]);
+        $table.="<tr>";
+        // $table.="<td>".$resultat[$i]['nom']."</td>";
+        // $table.="<td>".$resultat[$i]['prenom']."</td>";
+        foreach($resultat[$i] as $key=>$value) {
+            if ($key != "mdp"){
+                $table.="<td>".$value."</td>";
+            }
+        }
+        
+        $table.="</tr>";
+
+    }
+    $table.="</table>";
+
+    echo $table;
     
 }
 
